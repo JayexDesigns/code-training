4e25ea18fcd31bfa7b534f678a5bd56831cb4de6
Jayex <joaquintouron@gmail.com>
2023-03-13 22:15:05 -0100

    added one lists exercise

---
+++ jutge/lists/X59210/solution.cpp
@@ -0,0 +1,18 @@
+#include "llista.hpp"
+
+using namespace std;
+
+Llista::Llista(const vector<int> &v) {
+    _long = v.size();
+    _prim = NULL;
+    _prim = new node;
+    _prim->seg = NULL;
+    if (_long == 0) return;
+    node* pAux = _prim;
+    for (int i = 0; i < int(v.size()); ++i) {
+        pAux->seg = new node;
+        pAux->seg->info = v[i];
+        pAux->seg->seg = NULL;
+        pAux = pAux->seg;
+    }
+}
