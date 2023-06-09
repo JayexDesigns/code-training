d9d996b32ab9a56db0d44408eb7f52763b71c567
Jayex <joaquintouron@gmail.com>
2023-06-09 15:36:44 +0200

    added one leetcode exercise

---
+++ leetcode/2-Add-Two-Numbers.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* l3 = head;
+        bool carry = false;
+        while (l1 != nullptr || l2 != nullptr) {
+            int sum = 0;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            if (carry) ++sum;
+            if (sum >= 10) {
+                carry = true;
+                sum = sum % 10;
+            }
+            else carry = false;
+            l3->val = sum;
+            if (l1 != nullptr || l2 != nullptr) {
+                l3->next = new ListNode();
+                l3 = l3->next;
+            }
+        }
+        if (carry) {
+            l3->next = new ListNode();
+            l3->next->val = 1;
+        }
+        return head;
+    }
+};
