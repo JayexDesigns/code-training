b332c2454725baf6a51345f2f88137e44f05b102
Jayex <joaquintouron@gmail.com>
2023-07-11 18:43:28 +0200

    added one leetcode exercise

---
+++ leetcode/1431-Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max = 0;
+        for (int i = 0; i < candies.size(); ++i)
+            if (candies[i] > max) max = candies[i];
+        vector<bool> res(candies.size());
+        for (int i = 0; i < candies.size(); ++i) {
+            if (candies[i] + extraCandies < max) res[i] = false;
+            else res[i] = true;
+        }
+        return res;
+    }
+};
